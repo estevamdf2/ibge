fad0027ec8dae8d9c46fe452dd04467611c571cf
Marcos Estevam de Sousa <marcos.sousa@ebc.com.br>
2017-08-03 14:22:57 -0300

    Criação model Uf e Municipio

---
+++ src/main/java/br/com/example/model/Municipio.java
@@ -0,0 +1,21 @@
+package br.com.example.model;
+
+public class Municipio {
+
+	private String nome;
+	private String codigo;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	
+}
---
+++ src/main/java/br/com/example/model/Uf.java
@@ -0,0 +1,20 @@
+package br.com.example.model;
+
+public class Uf {
+
+	private String sigla;
+	private String codigo;
+	
+	public String getSigla() {
+		return sigla;
+	}
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	} 
+}
